28497ebbd69de3ee4a6e38e1ee705294a0372774
fiz3-sys <a.fiziadi3@gmail.com>
2025-10-07 21:14:57 +0700

    update js/dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -25,6 +25,7 @@ function tampilkanHalaman(halaman) {
 if (halaman=== "reppm") {
   judul.innerText = "Submission Pengantian Parts saat PM"
   konten.innerHTML ='<p> "Foto" </p>'
+  <script src="js/fotopm.js"></js>
 ;
  }   else if (halaman === "aktivitas") {
     judul.innerText = "Aktivitas Maintenance";
