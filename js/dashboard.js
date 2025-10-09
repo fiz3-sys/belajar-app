d128f064d091e857098824882b161cb68e5991f3
fiz3-sys <a.fiziadi3@gmail.com>
2025-10-10 06:30:54 +0700

    update js/dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -26,6 +26,26 @@ if (halaman=== "reppm") {
   judul.innerText = "Submission Pengantian Parts saat PM"
   konten.innerHTML = `
   Cari <input id="lapor" type="text"></input>
+  <select name="ordtype">
+        <option placeholder="All"></option>
+        <option value="PM01">PM01</option>
+        <option value="PM02">PM02</option>
+        <option value="PM03">PM03</option>
+    </select>
+    
+    <select name="area">
+      <option placeholder="All"></option>
+      <option value="FL1">Finishing 1</option>
+      <option value="FL2">Finishing 2</option>
+      <option value="CDG1">Continuous</option>
+      <option value="CDG2">Dryer</option>
+      <option value="CDG3">Glycerine</option>
+      <option value="CDG4">BBC Rawmat</option>
+      <option value="WWTP">Waste Water Treatment Plant</option>
+      <option value="IDGE">Electrical,Instrumentation, & Automation</option>
+      <option value="MMFE">Support Mechanical Fabrication Static & MHE</option>
+    </select>
+  </div>
    <table class="min-w-full border border-gray-200">
         <thead class="bg-gray-100">
           <tr>
