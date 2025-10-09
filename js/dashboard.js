24fc0e142cf27f7bdb9bc0ac2f7ad0d81bc9c219
fiz3-sys <a.fiziadi3@gmail.com>
2025-10-10 06:20:21 +0700

    update js/dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -25,29 +25,7 @@ function tampilkanHalaman(halaman) {
 if (halaman=== "reppm") {
   judul.innerText = "Submission Pengantian Parts saat PM"
   konten.innerHTML =
-    <table class="min-w-full border border-gray-200">
-        <thead class="bg-gray-100">
-          <tr>
-            <th class="px-4 py-2 border">Tanggal</th>
-            <th class="px-4 py-2 border">No Job</th>
-            <th class="px-4 py-2 border">Equipment</th>
-            <th class="px-4 py-2 border">Deskripsi</th>
-            <th class="px-4 py-2 border">Status</th>
-            <th class="px-4 py-2 border">Photo</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border px-4 py-2">2025-10-05</td>
-            <td class="border px-4 py-2">Job-1</td>
-            <td class="border px-4 py-2">Pump A1</td>
-            <td class="border px-4 py-2">Cek oli dan tekanan</td>
-            <td class="border px-4 py-2">Selesai</td>
-            <td class="border px-4 py-2"><input type="button" value="+"></input> 
-          </tr>
-        </tbody>
-      </table>
+   
  }   else if (halaman === "riwayat") {
     judul.innerText = "Aktivitas Maintenance";
     konten.innerHTML = `
