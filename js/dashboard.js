858ee983d94e2ecb73c36daefe4f249864906142
fiz3-sys <a.fiziadi3@gmail.com>
2025-10-10 06:25:11 +0700

    update js/dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -72,6 +72,7 @@ if (halaman=== "reppm") {
             <td class="border px-4 py-2">Pump A1</td>
             <td class="border px-4 py-2">Cek oli dan tekanan</td>
             <td class="border px-4 py-2">Selesai</td>
+            <td class="border px-4 py-2><a href="google.com">tanggal</a>
             
           </tr>
         </tbody>
